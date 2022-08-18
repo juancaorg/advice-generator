cce71409c2b9ac0424f854b38c48b6bb32cd5505
Juan Carrillo <juan@juanca.org>
2022-08-18 17:11:57 -0500

    style: format code

--- app.js
+++ app.js
@@ -13,17 +13,17 @@ function renderAdvice(adviceObj) {
 // and call renderAdvice(advice).
 async function fetchAdvice() {
 	try {
-		const response = await fetch(API_URL, { cache: 'no-cache' });
+		const response = await fetch(API_URL, { cache: "no-cache" });
 		const data = await response.json();
 		// data.slip is the adviceObj needed.
 		renderAdvice(data.slip);
-	} catch(error) {
+	} catch (error) {
 		console.log(error);
 	}
 }
 
 // Fetch a new advice everytime the button get pressed.
-fetchButton.addEventListener('click', fetchAdvice);
+fetchButton.addEventListener("click", fetchAdvice);
 
 // On load, fetch an advice.
 fetchAdvice();
